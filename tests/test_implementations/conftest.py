0baa58ec9a6a65b668b0ec39b73e873c5487f044
mfranzon <43796979+mfranzon@users.noreply.github.com>
2025-02-07 12:25:42 +0100

    remove fixture

--- tests/test_implementations/conftest.py
+++ tests/test_implementations/conftest.py
@@ -1,25 +1,5 @@
 import pytest
-import paramiko
 import subprocess
-
-
-@pytest.fixture
-def mock_ssh_client(monkeypatch):
-    """Mock paramiko.SSHClient."""
-
-    class MockSSHClient:
-        def set_missing_host_key_policy(self, policy):
-            pass
-
-        def connect(self, host, username, pkey):
-            if username == "fail":
-                raise paramiko.AuthenticationException("Auth failed")
-
-        def close(self):
-            pass
-
-    monkeypatch.setattr(paramiko, "SSHClient", MockSSHClient)
-    return MockSSHClient()
 
 
 @pytest.fixture
